60b0267a8195f249a5c53a5f0c3ec0fca31351ee
ChrisVaisnor <chrisvaisnor@gmail.com>
2022-08-09 19:15:44 -0700

    added everything

---
+++ create_files.py
@@ -0,0 +1,76 @@
+"""This script creates the input files for the sorting algorithms.
+The 15 files are defined as follows:
+
+    4 size files: 50, 1000, 2000, 5000, and 10000 integers.
+    3 versions per file: random, decending, and ascending.
+
+    Total of 15 files."""
+
+from random import randint, shuffle
+
+
+def create_file(filename: str, size: int, ascending: bool, random: bool) -> None:
+  """This function creates a file with the given parameters."""
+
+  with open(filename, 'w') as file:
+    if random:
+      array = list(range(size))
+      shuffle(array)
+      for i in range(len(array)):
+        if i == size - 1:
+          file.write(str(array[i]))
+        else:
+          file.write(str(array[i]) + '\n')
+    elif ascending:
+      for i in range(size):
+        if i == size - 1:
+          file.write(str(i))
+        else:
+          file.write(str(i) + '\n')
+    else:
+      for i in range(size):
+        if i == size - 1:
+          file.write(str(size - i))
+        else:
+          file.write(str(size - i) + '\n')
+      
+def create_files() -> None:
+  for size in [50, 1000, 2000, 5000, 10000]:
+    create_file('random_' + str(size) + '.txt', size, True, True)
+    create_file('descending_' + str(size) + '.txt', size, False, False)
+    create_file('ascending_' + str(size) + '.txt', size, True, False)
+
+
+def check_for_duplicates_in_file(filename: str) -> bool:
+  """This function checks for duplicates in a file."""
+
+  with open(filename, 'r') as file:
+    array = file.readlines()
+    for i in range(len(array)):
+      for j in range(i + 1, len(array)):
+        if array[i] == array[j]:
+          return True
+  return False
+
+
+def make_files_with_duplicates():
+  for size in [50, 1000, 2000, 5000, 10000]:
+    with open('test_random_' + str(size) + '.txt', 'w') as file:
+      for i in range(size):
+        file.write(str(randint(0, size)) + '\n')
+
+
+# checking for duplicates in the test files
+for size in [50, 1000, 2000, 5000, 10000]:
+  if check_for_duplicates_in_file('test_random_' + str(size) + '.txt'):
+    print('test_random_' + str(size) + '.txt has duplicates')
+  else:
+    print('test_random_' + str(size) + '.txt has no duplicates')
+
+# checking for duplicates in all other 'random' files
+for size in [50, 1000, 2000, 5000, 10000]:
+  if check_for_duplicates_in_file('random_' + str(size) + '.txt'):
+    print('random_' + str(size) + '.txt has duplicates')
+  else:
+    print('random_' + str(size) + '.txt has no duplicates')
+
--- lab4.py
+++ lab4.py
@@ -5,47 +5,19 @@
 # IDE: VS Code
 # Python Version: 3.8.10
 
+from sorting_functions import *
+
 def main():
-  
-  # Verification function for the sorted files
-  def verify_sorted(array: list) -> bool:
-    """This function verifies that the array is sorted in ascending order."""
+  """This is the main function for the program."""
 
-    for i in range(len(array) - 1):
-      if array[i] < array[i + 1]:
-        return False
-    return True
-
-  # partition the array, and return the index of the pivot
-  def partition(array: list, start: int, end: int) -> int:
-    """This function partitions the array around the pivot. The pivot is selected as the first item of each partition."""
-      
-    pivot = array[start]
-
- 
-    done = False
-    
-    # iterate through the array and swap items if they are less than the pivot
-    while (not done):
-      for i in range(start, end + 1):
-        if array[i] < pivot:
-          start += 1
-          break
-        else:
-          pivot < array[end]
-          end -= 1
-
-    # swap the pivot with the start index
-    array[start], array[end] = array[end], array[start]
-    
-    return start
+  # array of random numbers
+  array = [5,7,2,9,1,3,4,6,8]
+  start=0
+  end=len(array)-1
 
 
-  def quicksort_v1(array: list) -> list:
-    """This function performs the quicksort algorithm. Verison 1: pivot is the first element."""
-    
-    pivot = array[0] # select the first element as the pivot
-    pass
+  # partition the array around the pivot
+  pivot = partition_v1(array, start, end)
 
 
 if __name__ == '__main__':
@@ -53,5 +25,7 @@ if __name__ == '__main__':
   print('Programmer: Chris Vaisnor')
   print('IDE: VS Code')
   print('Python Version: 3.8.10')
+  print()
+  print('------------------------------------------------------------')
 
   main()
